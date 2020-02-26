563eeff88d4067f7ac23c35c5fe2fc24517962bf
Katrina_Dierking <kalynndier@gmail.com>
2020-02-26 00:36:30 -0500

    user-model.js created

---
+++ users/user-model.js
@@ -0,0 +1,32 @@
+const db = require('../database/dbConfig');
+
+function find() {
+    return db('users').select('id', 'username', 'password');
+}
+
+function findBy(filter) {
+    return db('users')
+    .where(filter);
+}
+
+function add(user) {
+    return db('users')
+    .insert(user, 'id')
+    .then (ids => {
+        const [id] = ids;
+        return findById(id);
+    });
+}
+
+function findById(id) {
+    return db('users')
+    .where ({id})
+    .first();
+}
+
+module.exports = {
+    add, 
+    find,
+    findBy,
+    findById
+}
