1bc9eee989cbd9f727814f20a610615b7876a71c
Katrina_Dierking <kalynndier@gmail.com>
2020-02-25 23:39:01 -0500

    installs done. Initial folders/files named

---
+++ api/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+require('dotenv').config();
+console.log(process.env.JWT_SECRET);
+
+// const authenticate = require('../auth/authenticate-middleware.js');
+const authRouter = require('../auth/auth-router.js');
+const apiRouter = require('./api-router');
+const usersRouter = require('../users/users-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+server.use('/api', apiRouter);
+server.use('/api/users', usersRouter);
+
+server.get('/', (req, res) => {
+    res.send("Is this thing on???")
+});
+
+module.exports = server;
