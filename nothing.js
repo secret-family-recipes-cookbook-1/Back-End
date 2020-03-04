0d58d53c62171c11c58ed692bb02aa451a6424aa
Katrina_Dierking <kalynndier@gmail.com>
2020-03-03 21:34:03 -0500

    changes to user-model

---
+++ nothing.js
@@ -0,0 +1,18 @@
+
+// router.put('/:id', restricted, (req, res) => {
+//   database('recipes')
+//   .where({id: req.params.id})
+
+//   .then(saved => {
+//     res.status(201).json({
+//         data: {
+//             user_id: req.body.user_id,
+//             title:req.body.title,
+//             source: req.body.source,
+//             instructions: req.body.instructions,
+//             category: req.body.category
+//             },
+//             ingredients: saved.ingredients.split(',')
+//         })
+//   });
+// })
